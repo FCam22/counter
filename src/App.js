eadacac2a3b6c12910b55b15e41ee4f650858055
unknown <cescanyc@msn.com>
2020-02-05 21:41:39 -0500

    Added CSS and created the increment method - tested and working :)

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ class Counter extends Component {
     }
   }
 
+  //this is where methods live (below constructor and above render)
+
+increment = ()=>{
+  this.setState ({
+    count: this.state.count + 1,
+  })
+}
+
+
   render(){
     return (
-      <div>
-
+      <div className="container">
+        <div className="navbar">COUNTER</div>
+          <div className="counter">
+            <h1> {this.state.count}</h1>
+            <button type="button" onClick={this.increment}>Increment</button>
+            <button type="button">Decrement</button>
+        </div>
       </div>
     )
   }
