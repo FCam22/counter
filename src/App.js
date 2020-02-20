637d60e14324e21585adb521ce56dcc2ca0cfd21
unknown <cescanyc@msn.com>
2020-02-19 20:09:20 -0500

    counter update 2/19/20

--- src/App.js
+++ src/App.js
@@ -5,16 +5,61 @@ class Counter extends Component {
     super();
     this.state = {
       count: 0,
+      doubleCount: false,
     }
   }
 
   //this is where methods live (below constructor and above render)
 
 increment = ()=>{
-  this.setState ({
+  if (this.state.count <20 ) {
+    this.setState({
     count: this.state.count + 1,
   })
 }
+if (this.state.doubleCount && this.state.count<19 ){
+    this.setState({
+      count: this.state.count + 2,
+    })
+  }
+}
+
+
+decrement = ()=>{
+  if (this.state.count >0 ) {
+    this.setState({
+    count: this.state.count - 1,
+  })
+}
+if (this.state.doubleCount){
+  if (this.state.count > 1){
+    this.setState({
+      count: this.state.count - 2,
+    })
+  }
+}
+}
+
+clear = ()=>{
+  this.setState({
+    count:0,
+  })
+}
+
+double = ()=>{
+  if (this.state.count) {
+    this.setState({
+    doubleCount: false,
+  })
+  }
+  else{
+    this.setState({
+      doubleCount: true,
+    })
+  }
+}
+
+
 
 
   render(){
@@ -23,12 +68,16 @@ increment = ()=>{
         <div className="navbar">COUNTER</div>
           <div className="counter">
             <h1> {this.state.count}</h1>
+            <button type="button" onClick={this.clear}>clear</button>
             <button type="button" onClick={this.increment}>Increment</button>
-            <button type="button">Decrement</button>
+            <button type="button" onClick={this.increment}>Decrement</button>
+            <button type="button" onClick={this.double}>{this.state.doubleCount ? 'Double Count' : 'Single Count'}</button>
+
         </div>
       </div>
     )
   }
-};
+}
+
 
 export default Counter;
